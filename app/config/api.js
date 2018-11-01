941898110eedbb1caf14be34030f53c07cf81cb7
Bert de Boer <bertde.boer@hotmail.com>
2018-11-01 10:53:03 +0100

    projecten dynamisch

--- app/config/api.js
+++ app/config/api.js
@@ -4,7 +4,7 @@ import { NetInfo } from "react-native";
 export default class Api {
   static instance = null;
 
-  url = "0.0.0.0:5000/";
+  url = "http://172.20.10.2:5000/";
 
   static getInstance() {
     if (Api.instance == null) {
@@ -15,37 +15,33 @@ export default class Api {
   }
 
   callApi(action, method, data, callBack = response => console.log(response)) {
-    NetInfo.getConnectionInfo().then(connectionInfo => {
-      if (connectionInfo.type != "none") {
-        if (method == "GET") {
-          fetch(this.url + action, {
-            method: method,
-            headers: {
-              "Content-Type": "application/json"
-            }
-          })
-            .then(response => response.json())
-            .then(responseJson => callBack(responseJson))
-            .catch(error => {
-              callBack(error);
-            });
-        } else if (method == "POST") {
-          fetch(this.url + action, {
-            method: method,
-            headers: {
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-          })
-            .then(response => response.json())
-            .then(responseJson => callBack(responseJson))
-            .catch(error => {
-              callBack(error);
-            });
+    console.log(" calling");
+    if (method == "GET") {
+      fetch(this.url + action, {
+        method: method,
+        headers: {
+          "Content-Type": "application/json"
         }
-      } else {
-        callBack({ responseCode: 503 });
-      }
-    });
+      })
+        .then(response => response.json())
+        .then(responseJson => callBack(responseJson))
+        .catch(error => {
+          callBack(error);
+        });
+    } else if (method == "POST") {
+      console.log(this.url + action);
+      fetch(this.url + action, {
+        method: method,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(response => response.json())
+        .then(responseJson => callBack(responseJson))
+        .catch(error => {
+          callBack(error);
+        });
+    }
   }
 }
